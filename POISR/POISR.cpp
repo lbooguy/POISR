f8380173182f1bf6f8a74aec3e76f8778464d38d
lbooguy <bugai.oa18@physics.msu.ru>
2024-03-14 03:18:12 +0300

    Добавьте файлы проекта.

---
+++ POISR/POISR.cpp
@@ -0,0 +1,24 @@
+﻿#include <opencv2/opencv.hpp> 
+#include <iostream> 
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	// Read the image file 
+	Mat image = imread("C:/Users/USER/Pictures/Camera Roll/WIN_20231112_17_52_12_Pro.jpg");
+	// Check for failure 
+	if (image.empty())
+	{
+		cout << "Image Not Found!!!" << endl;
+		cin.get(); //wait for any key press 
+		return -1;
+	}
+	// Show our image inside a window. 
+	imshow("Image Window Name here", image);
+
+	// Wait for any keystroke in the window 
+	waitKey(0);
+	return 0;
+}
