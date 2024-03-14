9f2f5f866d1bcf744bd9e62f554996fbf7337962
lbooguy <bugai.oa18@physics.msu.ru>
2024-03-14 05:42:40 +0300

    add Canny in second window

--- POISR/task1.cpp
+++ POISR/task1.cpp
@@ -7,7 +7,8 @@ using namespace std;
 int g_slider_position = 0;
 int g_run = 1, g_dontset = 0; // начинаем в режиме покадрового просмотра
 cv::VideoCapture g_cap;
-
+cv::VideoCapture g_gray;
+cv::VideoCapture g_canny;
 
 void onTrackbarSlide(int pos, void*) {
 	g_cap.set(cv::CAP_PROP_POS_FRAMES, pos);
@@ -17,14 +18,15 @@ void onTrackbarSlide(int pos, void*) {
 }
 
 int main(int argc, char** argv) {
-	cv::namedWindow("Example2_4", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("source", cv::WINDOW_NORMAL);
+	cv::namedWindow("Canny", cv::WINDOW_NORMAL);
 	g_cap.open("C:/Users/USER/Videos/video.mp4");
 	int frames = (int)g_cap.get(cv::CAP_PROP_FRAME_COUNT);
 	int tmpw = (int)g_cap.get(cv::CAP_PROP_FRAME_WIDTH);
 	int tmph = (int)g_cap.get(cv::CAP_PROP_FRAME_HEIGHT);
 	cout << "В виде " << frames << " кадров размером("
 		<< tmpw << ", " << tmph << ")." << endl;
-	cv::createTrackbar("Position", "Example2_4", &g_slider_position, frames,
+	cv::createTrackbar("Position", "source", &g_slider_position, frames,
 		onTrackbarSlide);
 	cv::Mat frame;
 	for (;;) {
@@ -32,8 +34,13 @@ int main(int argc, char** argv) {
 			g_cap >> frame; if (frame.empty()) break;
 			int current_pos = (int)g_cap.get(cv::CAP_PROP_POS_FRAMES);
 			g_dontset = 1;
-			cv::setTrackbarPos("Position", "Example2_4", current_pos);
-			cv::imshow("Example2_4", frame);
+			cv::setTrackbarPos("Position", "source", current_pos);
+			cv::imshow("source", frame);
+			cv::resizeWindow("source", 500, 500);
+			cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+			cv::Canny(frame, frame, 10, 100, 3, true);
+			cv::imshow("Canny", frame);
+			cv::resizeWindow("Canny", 500, 500);
 			g_run -= 1;
 		}
 		char c = (char)cv::waitKey(10);
